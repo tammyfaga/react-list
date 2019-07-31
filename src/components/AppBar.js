7c6bcb6cff525be5f17114de1ed5abba2f84a7b5
Tammy Fagá <tammyfaga@yahoo.com.br>
2019-07-31 00:03:08 -0300

    tarefa: Adiciona menu de navegação lateral

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const AppBar = ({ isLoading, saveHasError, onSaveRetry }) => (
+const AppBar = ({ isLoading, saveHasError, onSaveRetry, onOpenMenu }) => (
   <div className="app-bar">
     <div className="app-bar__container">
+      <button className="app-bar__action" onClick={onOpenMenu}>
+        <i className="material-icons">menu</i>
+      </button>
+
       <span className="app-bar__brand">React List</span>
       {isLoading && (
         <button className="app-bar__action app-bar__action--rotation">
           <i className="material-icons">refresh</i>
         </button>
       )}
+
       {saveHasError && (
         <button className="app-bar__action app-bar__action--danger" onClick={onSaveRetry}>
           <i className="material-icons">cloud_off</i>
