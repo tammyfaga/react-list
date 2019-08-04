9d5d0de4feffbec9b0534d0244fa034edd00c10b
Tammy Fagá <tammyfaga@yahoo.com.br>
2019-08-04 20:31:54 -0300

    tarefa: Adiciona rotas com react-router-dom

--- src/components/NavigationDrawer.js
+++ src/components/NavigationDrawer.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import className from 'classnames';
+import { withRouter } from "react-router-dom";
 
-const NavigationDrawer = ({ isOpen, onCloseMenu }) => (
+const NavigationDrawer = ({ isOpen, onCloseMenu, history}) => (
   <div 
     className={className("navigation-drawer", {
       "navigation-drawer--open": isOpen 
     })}
   >
     <div className="navigation-drawer__head">
-      <button className="navigation-drawer__head__button" onClick={onCloseMenu}>
+      <button className="navigation-drawer__head__button" 
+        onClick={onCloseMenu}>
         <i className="material-icons">close</i>
       </button>
     </div>
     <div className="navigation-drawer__menu">
-      <button className="navigation-drawer__menu__item">
+      <button className="navigation-drawer__menu__item"
+        onClick={() => {
+          onCloseMenu();
+          history.push("/"); 
+        }}
+      >
         <i className="material-icons">note</i> Notas
       </button>
-      <button className="navigation-drawer__menu__item">
+      <button className="navigation-drawer__menu__item"
+        onClick={() => {
+          onCloseMenu();
+          history.push("/about"); 
+        }}
+      >
         <i className="material-icons">info</i> Sobre
       </button>
     </div>
   </div>
 );
 
-export default NavigationDrawer;
+export default withRouter(NavigationDrawer);
