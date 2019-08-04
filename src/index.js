1711f81a15306d144b67355b4a2c8a0c99c89fd6
tammy <tmata@recordtv.com.br>
2019-08-04 20:31:54 -0300

    tarefa: Adiciona rotas com react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./index.scss";
 
 import App from "./components/App";
+import About from "./components/About";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <React.Fragment>
+      <Route path="/" exact component={App} />
+      <Route path="/about" exact component={About} />
+    </React.Fragment>
+  </Router>,
+  document.getElementById("root")
+);
