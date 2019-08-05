ef641b082c76c69d5cac3185413933491077158a
Tammy Fagá <tammyfaga@yahoo.com.br>
2019-08-04 21:02:56 -0300

    correção: Layout

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./index.scss";
 
 import App from "./components/App";
-import About from "./components/About";
 
 ReactDOM.render(
-  <Router>
-    <React.Fragment>
-      <Route path="/" exact component={App} />
-      <Route path="/about" exact component={About} />
-    </React.Fragment>
-  </Router>,
+  <App />,
   document.getElementById("root")
 );
