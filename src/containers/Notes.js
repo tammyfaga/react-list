fd79f5ff0e6a69d529764aee4c61b0f68350267a
Tammy Fagá <tammyfaga@yahoo.com.br>
2019-08-04 22:31:45 -0300

    tarefa: Adiciona diretorio de containers para organização do projeto

---
+++ src/containers/Notes.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+
+import NewNote from '../components/NewNote';
+import NoteList from '../components/NoteList';
+import Error from '../components/Error';
+
+const Notes = ({ 
+  notes,
+  reloadHasError,
+  onRetry,
+  onAddNote,
+  onDelete,
+  onMove,
+  onEdit  
+}) => {
+  if (reloadHasError) {
+    return <Error onRetry={onRetry} />;
+  }
+
+  return (
+    <Fragment>
+      <NewNote onAddNote={onAddNote} />
+      <NoteList 
+        notes={notes} 
+        onDelete={onDelete}
+        onMove={onMove} 
+        onEdit={onEdit}
+      />
+    </Fragment>
+  );
+};
+
+export default Notes;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-import App from "./components/App";
+import App from "./containers/App";
 
 ReactDOM.render(
   <App />,
