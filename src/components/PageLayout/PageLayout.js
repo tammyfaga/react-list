199bcea5a2dfd0baa3967da2a2d5bc0c704c942b
tammy <tmata@recordtv.com.br>
2019-08-07 23:50:08 -0300

    refatoração: Ajustes de layout

---
+++ src/components/PageLayout/PageLayout.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import AppBar from "../AppBar/AppBar";
+import NavigationDrawer from "../NavigationDrawer/NavigationDrawer";
+
+import "./page-layout.scss";
+
+const PageLayout = ({ 
+  children,
+  isLoading,
+  saveHasError,
+  onSaveRetry,
+  onOpenMenu,
+  isMenuOpen,
+  onCloseMenu 
+}) => {
+  <div>
+    <AppBar 
+      isLoading={isLoading} 
+      saveHasError={saveHasError} 
+      onSaveRetry={onSaveRetry}
+      onOpenMenu={onOpenMenu}
+    />
+    <div className="container">
+      {children}
+    </div>
+    <NavigationDrawer 
+      isOpen={isMenuOpen}
+      onCloseMenu={onCloseMenu}
+    />
+  </div>
+};
+
+export default PageLayout;
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
-import AppBar from "./AppBar/AppBar";
-import NavigationDrawer from "./NavigationDrawer/NavigationDrawer";
 import Error from "./Error/Error";
 import NewNote from "./NewNote/NewNote";
 import Note from "./Note/Note";
 import NoteList from "./NoteList/NoteList";
+import PageLayout from "./PageLayout/PageLayout";
 
-export { AppBar, NavigationDrawer, Error, NewNote, Note, NoteList };
+export { Error, NewNote, Note, NoteList, PageLayout };
