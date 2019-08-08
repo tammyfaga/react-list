05f8ecec94c81edb0586ce9dcaad51afdf42a1f2
Tammy Fagá <tammyfaga@yahoo.com.br>
2019-08-08 00:12:19 -0300

    tarefa: Cria componente Header

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import NewNote from "./NewNote/NewNote";
 import Note from "./Note/Note";
 import NoteList from "./NoteList/NoteList";
 import PageLayout from "./PageLayout/PageLayout";
+import Header from "./Header/Header";
 
-export { Error, NewNote, Note, NoteList, PageLayout };
+export { Error, NewNote, Note, NoteList, PageLayout, Header };
