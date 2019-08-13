e58856b6e814845b70b9c20bbcc1ca59aabdd81a
tammy <tmata@recordtv.com.br>
2019-08-12 22:27:24 -0300

    tarefa: Cria componentes Button e ButtonLink

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import "./button.scss";
+
+const Button = ({ children, onClick }) => (
+  <button className="button" onClick={onClick}>
+    {children}
+  </button>
+
+);
+
+export default Button;
---
+++ src/components/Button/ButtonLink.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./button.scss";
+
+const ButtonLink = ({ children, to }) => (
+  <Link className="button" to={to}>
+    {children}
+  </Link>
+
+);
+
+export default ButtonLink;
