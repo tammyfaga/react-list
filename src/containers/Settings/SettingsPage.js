df71997744dcf78545c05b3ffb4a6623da6e7137
Tammy Fagá <tammyfaga@yahoo.com.br>
2019-08-14 23:13:27 -0300

    refatoração: Higher-order components

--- src/containers/Settings/SettingsPage.js
+++ src/containers/Settings/SettingsPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Header } from "../../components";
-import SettingsContext from "./SettingsContext";
+import withSettings from "./withSettings";
 
 import "./settings.scss";
 
@@ -10,33 +10,29 @@ const themes = [
   { key: "classic", label: "Clássico", colorPrimary: "#795548" }
 ];
 
-const SettingsPage = () => (
+const SettingsPage = ({ theme : selectedTheme, toggleTheme }) => (
   <div>
     <Header>Temas</Header>
-    <SettingsContext.Consumer>
-      {({ theme : selectedTheme, toggleTheme }) => (
-        <div className="themes">
-          {themes.map(theme => (
-            <button 
-              key={theme.key}
-              className="themes__item" 
-              style={{ backgroundColor: theme.colorPrimary}}
-              onClick={() => { 
-                toggleTheme(theme);
-              }}       
-            >
-              <p>
-                {theme.label}
-                {theme.key === selectedTheme.key && (
-                  <i className="material-icons">check</i>
-                )}
-              </p>
-            </button>  
-          ))}
-        </div>
-        )}
-    </SettingsContext.Consumer>
+    <div className="themes">
+      {themes.map(theme => (
+        <button 
+          key={theme.key}
+          className="themes__item" 
+          style={{ backgroundColor: theme.colorPrimary}}
+          onClick={() => { 
+            toggleTheme(theme);
+          }}       
+        >
+          <p>
+            {theme.label}
+            {theme.key === selectedTheme.key && (
+              <i className="material-icons">check</i>
+            )}
+          </p>
+        </button>  
+      ))}
+    </div>
   </div>
 );
 
-export default SettingsPage;
+export default withSettings(SettingsPage);
