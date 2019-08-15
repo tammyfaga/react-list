73f99d0eb0136475a097821058d667d7e5ac2379
Tammy Fagá <tammyfaga@yahoo.com.br>
2019-08-14 21:59:41 -0300

    tarefa: Reutilizando o contexto

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,28 +1,34 @@
 import React from 'react';
 
+import SettingsContext from "../../containers/Settings/SettingsContext";
+
 import "./app-bar.scss";
 
 const AppBar = ({ isLoading, saveHasError, onSaveRetry, onOpenMenu }) => (
-  <div className="app-bar">
-    <div className="app-bar__container">
-      <button className="app-bar__action" onClick={onOpenMenu}>
-        <i className="material-icons">menu</i>
-      </button>
+  <SettingsContext.Consumer>
+    {({ theme }) => (
+      <div className="app-bar" style={{ backgroundColor: theme.colorPrimary }}>
+        <div className="app-bar__container">
+          <button className="app-bar__action" onClick={onOpenMenu}>
+            <i className="material-icons">menu</i>
+          </button>
 
-      <span className="app-bar__brand">React List</span>
-      {isLoading && (
-        <button className="app-bar__action app-bar__action--rotation">
-          <i className="material-icons">refresh</i>
-        </button>
-      )}
+          <span className="app-bar__brand">React List</span>
+          {isLoading && (
+            <button className="app-bar__action app-bar__action--rotation">
+              <i className="material-icons">refresh</i>
+            </button>
+          )}
 
-      {saveHasError && (
-        <button className="app-bar__action app-bar__action--danger" onClick={onSaveRetry}>
-          <i className="material-icons">cloud_off</i>
-        </button>
-      )}
-    </div>
-  </div>
+          {saveHasError && (
+            <button className="app-bar__action app-bar__action--danger" onClick={onSaveRetry}>
+              <i className="material-icons">cloud_off</i>
+            </button>
+          )}
+        </div>
+      </div>
+    )}
+  </SettingsContext.Consumer>
 );
 
 export default AppBar;
