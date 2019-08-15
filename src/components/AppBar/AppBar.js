d7fd98646bb1b5c9aa93a4cc996447f9439d386c
tammy <tmata@recordtv.com.br>
2019-08-14 23:13:27 -0300

    refatoração: Higher-order components

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,34 +1,30 @@
 import React from 'react';
 
-import SettingsContext from "../../containers/Settings/SettingsContext";
+import withSettings from "../../containers/Settings/withSettings";
 
 import "./app-bar.scss";
 
-const AppBar = ({ isLoading, saveHasError, onSaveRetry, onOpenMenu }) => (
-  <SettingsContext.Consumer>
-    {({ theme }) => (
-      <div className="app-bar" style={{ backgroundColor: theme.colorPrimary }}>
-        <div className="app-bar__container">
-          <button className="app-bar__action" onClick={onOpenMenu}>
-            <i className="material-icons">menu</i>
-          </button>
+const AppBar = ({ isLoading, saveHasError, onSaveRetry, onOpenMenu, theme }) => (
+  <div className="app-bar" style={{ backgroundColor: theme.colorPrimary }}>
+    <div className="app-bar__container">
+      <button className="app-bar__action" onClick={onOpenMenu}>
+        <i className="material-icons">menu</i>
+      </button>
 
-          <span className="app-bar__brand">React List</span>
-          {isLoading && (
-            <button className="app-bar__action app-bar__action--rotation">
-              <i className="material-icons">refresh</i>
-            </button>
-          )}
+      <span className="app-bar__brand">React List</span>
+      {isLoading && (
+        <button className="app-bar__action app-bar__action--rotation">
+          <i className="material-icons">refresh</i>
+        </button>
+      )}
 
-          {saveHasError && (
-            <button className="app-bar__action app-bar__action--danger" onClick={onSaveRetry}>
-              <i className="material-icons">cloud_off</i>
-            </button>
-          )}
-        </div>
-      </div>
-    )}
-  </SettingsContext.Consumer>
+      {saveHasError && (
+        <button className="app-bar__action app-bar__action--danger" onClick={onSaveRetry}>
+          <i className="material-icons">cloud_off</i>
+        </button>
+      )}
+    </div>
+  </div>
 );
 
-export default AppBar;
+export default withSettings(AppBar);
