947235f6ff50a1bc9db83a3ea30d9f8482ec0902
tammy <tmata@recordtv.com.br>
2019-08-14 22:39:20 -0300

    refatoração: Separa os estados para SettingsProvider

---
+++ src/containers/Settings/SettingsProvider.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import SettingsContext from "./SettingsContext";
+
+class SettingsProvider extends Component {
+  state = { theme: {} };
+
+  handleToggleTheme = theme => {
+    this.setState({ theme });
+  };
+
+  render() {
+    const { theme } = this.state;
+
+    return (
+      <SettingsContext.Provider 
+        value={{ theme, toggleTheme: this.handleToggleTheme }}
+      >
+        {this.props.children}
+      </SettingsContext.Provider>
+    );
+  }
+}
+
+export default SettingsProvider;
